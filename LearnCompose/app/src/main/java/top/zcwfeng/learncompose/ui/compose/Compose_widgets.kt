d925ed1ac3486447e37180577b0ff83072ba8a6f
zcw <zcwfeng@126.com>
2023-05-19 16:08:39 +0800

    8. 通用组件Compose_widgets.kt

---
+++ LearnCompose/app/src/main/java/top/zcwfeng/learncompose/ui/compose/Compose_widgets.kt
@@ -0,0 +1,117 @@
+package top.zcwfeng.learncompose.ui.compose
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.Icon
+import androidx.compose.material3.TabPosition
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import top.zcwfeng.learncompose.R
+import top.zcwfeng.learncompose.ui.theme.LightBlue
+import top.zcwfeng.learncompose.ui.theme.Purple200
+import top.zcwfeng.learncompose.ui.theme.Purple700
+
+//<editor-fold desc="TabPage">
+private enum class TabPage {
+    Home, Work
+}
+
+@Composable
+private fun HomeTabBar(
+    backgroundColor: Color,
+    tabPage: TabPage,
+    onTabSelected: (tabPage: TabPage) -> Unit
+) {
+    TabRow(
+        selectedTabIndex = tabPage.ordinal,
+        containerColor = backgroundColor,
+        indicator = { tabPositions ->
+            HomeTabIndicator(tabPositions, tabPage)
+        }
+    ) {
+        HomeTab(
+            icon = Icons.Default.Home,
+            title = stringResource(R.string.home),
+            onClick = { onTabSelected(TabPage.Home) }
+        )
+        HomeTab(
+            icon = Icons.Default.AccountBox,
+            title = stringResource(R.string.work),
+            onClick = { onTabSelected(TabPage.Work) }
+        )
+    }
+}
+
+
+@Composable
+private fun HomeTab(
+    icon: ImageVector,
+    title: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            imageVector = icon,
+            contentDescription = null
+        )
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(text = title)
+    }
+}
+
+@Composable
+private fun HomeTabIndicator(
+    tabPositions: List<TabPosition>,
+    tabPage: TabPage
+) {
+    // TODO 4: Animate these value changes.
+    val indicatorLeft = tabPositions[tabPage.ordinal].left
+    val indicatorRight = tabPositions[tabPage.ordinal].right
+    val color = if (tabPage == TabPage.Home) Purple700 else Purple200
+    Box(
+        Modifier
+            .fillMaxSize()
+            .wrapContentSize(align = Alignment.BottomStart)
+            .offset(x = indicatorLeft)
+            .width(indicatorRight - indicatorLeft)
+            .padding(4.dp)
+            .fillMaxSize()
+            .border(
+                BorderStroke(2.dp, color),
+                RoundedCornerShape(4.dp)
+            )
+    )
+}
+
+@Preview
+@Composable
+private fun PreviewHomeTabBar() {
+    HomeTabBar(
+        backgroundColor = Purple200,
+        tabPage = TabPage.Home,
+        onTabSelected = {}
+    )
+}
+
+//</editor-fold>
